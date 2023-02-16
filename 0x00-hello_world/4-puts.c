c5babdb275b04b1f8de547270f6809255f01e72e
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-16 12:35:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * printing a string using pusts
  */
 int main(void)
-{
-	/*
-	 * printing a string using pusts
- 	*/
-
+}
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
