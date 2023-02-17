bb2f5cbc86968f1f41c453cdffdb16b61a00ce79
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-17 12:31:43 -0800

    hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 */
 int main(void)
 {
-char az;
-char AZ;
-for (az = 'a' ; az <= 'z' ; az++)
+	char az;
+	char AZ;
+	for (az = 'a' ; az <= 'z' ; az++)
 {
         putchar(az);
 }
-for (AZ = 'A' ; AZ <= 'Z' ; AZ++)
+	for (AZ = 'A' ; AZ <= 'Z' ; AZ++)
 {
 	putchar(AZ);
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
