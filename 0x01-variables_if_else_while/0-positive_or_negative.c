04d30c4cd9ac11077b5d1cf749cf789cfa6ed8b4
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-17 11:40:24 -0800

    hot fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,15 +20,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
+	} else if (n == 0)
         {
                 printf("%i is zero\n", n);
-        }
-	else if (n < 0)
+        } else if (n < 0)
         {
                 printf("%i is negative\n", n);
         }
-
 	return (0);
 }
