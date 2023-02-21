333e25c0ef1681d429fc885f727cb70433492a59
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-21 08:31:59 -0800

    Hot Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,11 @@
 int main(void){
 
 char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
 unsigned int c;
-for (c = 0; c < sizeof(c); c++){
+
+for (c = 0; c < sizeof(x); c++)
+{
 	_putchar(x[c]);
 }
 
