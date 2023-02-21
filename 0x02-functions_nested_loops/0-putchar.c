dc19acef9aa640863ce277eed74ba15cb966386e
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-21 08:28:22 -0800

    Hot Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,15 @@
 
 int main(void){
 
-	char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	int c;
-	for (c = 0; c < sizeof(c); c++){
-		_putchar(x[c]);
-	}
-
-	
-
-	_putchar('\n');
-	return (0);
+char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+unsigned int c;
+for (c = 0; c < sizeof(c); c++){
+	_putchar(x[c]);
 }
+
+
+
+_putchar('\n');
+return (0);
+}
+
