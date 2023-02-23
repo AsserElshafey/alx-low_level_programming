9c6bf79adddf89375b8baf02ff81a56d0d08d444
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-23 07:58:30 -0800

    HOT FIX

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - numbers from 1 to 10
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
