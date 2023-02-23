b9d07ce5cac7a4ea102091bbcc94ef32e717796a
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-23 07:43:55 -0800

    HOT FIX

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * main - check if a number from 1 to 9
+ *
+ * @_isdigit: return 1 if a number else 0
+ *
+ * Rrturn: always 0
+ */
+int _isdigit(inc c)
+{
+	if ((c >=0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
