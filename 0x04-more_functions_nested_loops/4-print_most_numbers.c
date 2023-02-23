da7bcf24354c8033260a3e0e832ff8c2237a80f9
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-23 08:19:01 -0800

    HOT FIX

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
@@ -9,12 +9,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((i == 50) || (i == 52))
 			continue;
 		else
-			_putchar(i + '0');
+			_putchar(i);
 	}
 	_putchar('\n');
 }
