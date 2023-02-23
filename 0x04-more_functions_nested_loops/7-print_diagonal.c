1f0754cd5d14d762ee59f44e527dbd233506a730
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-23 08:59:49 -0800

    HOT FIX

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a straight line in the terminal
+ * @n: input number of times to print '\'
+ * Return: a straight line
+ */
+
+void print_diagonal(int n)
+{
+	int yo, lo;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		for (yo = 1; yo <= n; yo++)
+		{
+			for (lo = 1; lo < yo; lo++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+	
+
