ca70ad120d0cd4689c911cdc651855d21810c444
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-27 11:52:48 -0800

    pointers & arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
-		if (i = '\0')
-			dest[i] = '\0';
+		&dest[i] = &src[i];
 	}
 	return (dest);
 }
