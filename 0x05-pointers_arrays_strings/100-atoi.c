a77e8cc6cb9e1d201f1e9e1e92b74d7371013e7d
Asser Elshafey <elshafeyasser@yahoo.com>
2023-02-27 12:21:47 -0800

    pointers & arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _atoi - Entry point
@@ -7,6 +8,17 @@
  */
 int _atoi(char *s)
 {
+	int res = 0;
+	int sign = 1;
+	int i = 0;
 
-	return (0);
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; s[i] != ''; ++i)
+		res = res * 10 + s[i] - '0';
+
+	return (sign * res);
 }
