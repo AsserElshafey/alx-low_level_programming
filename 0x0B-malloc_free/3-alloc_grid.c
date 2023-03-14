7d55aaa7dee11979cecad055089d2ede220ae333
Asser Elshafey <elshafeyasser@yahoo.com>
2023-03-14 12:33:36 -0700

    malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,22 +16,22 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	iarray = malloc(sizeof(int) * height);
-
 	if (iarray == NULL)
-	{
-		free(iarray);
 		return (NULL);
-	}
-
 	for (i = 0; i < height; i++)
 	{
 		iarray[i] = malloc(sizeof(int) * width);
-		if (iarray == NULL)
+		if (iarray[i] == NUll)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(iarray[j]);
+			}
 			free(iarray);
-			return (NULL);
 		}
-
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (n = 0; n < width; n++)
 		{
 			iarray[i][n] = 0;
