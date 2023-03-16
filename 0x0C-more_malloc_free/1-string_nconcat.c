9a3bc2c614dbc11b44be34eb61f37250dedcf504
Asser Elshafey <elshafeyasser@yahoo.com>
2023-03-16 08:30:57 -0700

    More_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+unsigned int length(char *s1);
+
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - Concats two strings up to n bytes of the second one
+ * @s1: First string
+ * @s2: Second string
+ * @n: number of bytes
+ * Return: Pointer to a char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	int i;
+	unsigned int j;
+	unsigned int len;
+	char *heap_array;
 
+	/* Initializing to an empty string if value is NULL*/
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
+	if (s1 == NULL)
 		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	len = length(s1);
+	heap_array = malloc((len + n + 1) * sizeof(char));
+	if (heap_array == NULL)
 		return (NULL);
+	len = length(s2);
+	if (n >= len)
+		n = len;
+	/*Populating the new array with values*/
+	for (i = 0; s1[i] != '\0'; i++)
+		heap_array[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		heap_array[i] = s2[j];
+	heap_array[i] = '\0';
+	return (heap_array);
+}
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+/**
+ * length - Calculates the length of a string
+ * @s1: The string
+ * Return: The length of a string as an integer
+ */
+unsigned int length(char *s1)
+{
+	unsigned int i;
+
+	if (s1 == NULL)
+		return (0);
+	for (i = 0; s1[i] !=  '\0'; i++)
+	{}
+
+	return (i);
 }
