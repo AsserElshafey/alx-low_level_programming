4f270a3139939d22f42dc31c7ef14cf0fa7b49db
Asser Elshafey <elshafeyasser@yahoo.com>
2023-04-09 13:11:10 -0700

    HOT FIX

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x = 0;
-	int y = 1, i = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i + 1])
+	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		i++;
-	}
+		
+		x = (x << 1) + (b[i] - '0');
 
-	while (i >= 0)
-	{
-		x += ((b[i] - '0') * y);
-		y *= 2;
-		i--;
+		i++;
 	}
 
 
