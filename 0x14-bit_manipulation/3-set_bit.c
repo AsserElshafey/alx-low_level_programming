c614080cce191d4d5d24c0701f3272383ab61969
Asser Elshafey <elshafeyasser@yahoo.com>
2023-04-09 14:20:04 -0700

    HOT FIX

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,12 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
-
-	unsigned long int mask = 1UL << index;
-
-	*n = *n | mask;
-
-	return (1);
+	}else if
+	{
+		unsigned long int mask = 1UL << index;
+		*n = *n | mask;
+		return (1);
+	}
 }
