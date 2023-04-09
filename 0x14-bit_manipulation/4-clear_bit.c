24245ad1977c4e48ec4fd9abddb2ab23e3e828d8
Asser Elshafey <elshafeyasser@yahoo.com>
2023-04-09 14:51:12 -0700

    HOT FIX

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * set_bit - Entry Point
+ * clear_bit - Entry Point
  * @n: input
  * @index: index
  * Return: 0
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int mask;
+	unsigned long int mask;
 
-        if (index >= sizeof(*n) * 8)
-                return (-1);
+	if (index >= sizeof(*n) * 8)
+		return (-1);
 
-
-        mask = ~ (1UL << index);
-        *n = *n & mask;
-        return (1);
+	mask = ~(1UL << index);
+	*n = *n & mask;
+	return (1);
 }
