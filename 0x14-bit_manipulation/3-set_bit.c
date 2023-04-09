377b8ba60a343a2cf7456d431af9e0d8589bc617
Asser Elshafey <elshafeyasser@yahoo.com>
2023-04-09 14:17:29 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * set_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	unsigned long int mask = 1UL << index;
+
+	*n = *n | mask;
+
+	return (1);
+}
